d3a39e9eba2e8d79bad067b63e00b71719a09b3e
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2023-06-07 16:08:55 -0400

    Update UpcomingWeather.js

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -2,56 +2,16 @@ import React from 'react'
 import {
   SafeAreaView,
   View,
-  Text,
   StyleSheet,
   FlatList,
   ImageBackground,
   StatusBar
 } from 'react-native'
-import { Entypo } from '@expo/vector-icons'
-import { Feather } from '@expo/vector-icons'
 import ListItem from '../components/ListItem'
 
-const DATA = [
-  {
-    dt_txt: '2022-08-30 16:00:00',
-    main: {
-      temp_min: 296.34,
-      temp_max: 298.24
-    },
-    weather: [
-      {
-        main: 'Rain'
-      }
-    ]
-  },
-  {
-    dt_txt: '2023-02-30 6:00:00',
-    main: {
-      temp_min: 126.34,
-      temp_max: 228.24
-    },
-    weather: [
-      {
-        main: 'Snow'
-      }
-    ]
-  },
-  {
-    dt_txt: '2022-12-30 12:00:00',
-    main: {
-      temp_min: 111.34,
-      temp_max: 142.24
-    },
-    weather: [
-      {
-        main: 'Sunny'
-      }
-    ]
-  }
-]
+// This shits broke >:|
 
-const UpcomingWeather = () => {
+const UpcomingWeather = ({ weatherData }) => {
   const renderItem = ({ item }) => (
     <ListItem
       condition={item.weather[0].main}
@@ -64,14 +24,13 @@ const UpcomingWeather = () => {
   const { container, image } = styles
   return (
     <SafeAreaView style={container}>
-      <Text>UpcomingWeather</Text>
-      {/* <ImageBackground source={require('../../assets/bacon.png')} style={image}> */}
-      <FlatList>
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.dt_txt}
-      </FlatList>
-      {/* </ImageBackground> */}
+      <ImageBackground source={require('../../assets/bacon.png')} style={image}>
+        <FlatList>
+          data={weatherData}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.dt_txt}
+        </FlatList>
+      </ImageBackground>
     </SafeAreaView>
   )
 }
@@ -80,14 +39,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
     backgroundColor: '#fff'
   },
   image: {
-    flex: 1,
-    height: 395,
-    width: 395
+    flex: 1
+    // height: 395,
+    // width: 395
   }
 })
 
