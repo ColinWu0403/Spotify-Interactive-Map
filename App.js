12633cd2417d2865bfa3d11e34668f94553bc0e1
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2023-06-26 13:44:08 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,41 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { ActivityIndicator, View, StyleSheet } from 'react-native'
-import Tabs from './src/components/Tabs'
-import * as Location from 'expo-location'
-import { WEATHER_API_KEY } from '@env'
-import { useGetWeather } from './src/hooks/useGetWeather'
-import ErrorItem from './src/components/ErrorItem'
-
-// The app
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import MusicPlayer from './src/screens/MusicPlayer'
 
 const App = () => {
-  const [loading, error, weather] = useGetWeather()
-
-  console.log(loading, error, weather)
-
-  if (weather && weather.list) {
-    return (
-      <NavigationContainer>
-        <Tabs weather={weather} />
-      </NavigationContainer>
-    )
-  }
-
   return (
-    <View style={styles.container}>
-      {loading ? (
-        <ActivityIndicator size={'large'} color={'green'} />
-      ) : (
-        <ErrorItem />
-      )}
+    <View style={style.container}>
+      <MusicPlayer />
+      {/* <Text>eee</Text> */}
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    justifyContent: 'center',
     flex: 1
   }
 })
